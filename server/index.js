9bb2843cda30c3c638d7f429d98cb3c41bbac158
Erik Todorov <eriktdrv@gmail.com>
2022-11-25 17:42:42 +0200

    Adding cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require('./configs/cors');
 
 const server = require('./environment');
 const initDatabase = require('./configs/database');
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    console.log(req.user)
+    req.headers['X-Authorization'] = 'asdjpoiasdqi9h402rjpm'
+    console.log(req.headers)
     res.end()
 })
     
