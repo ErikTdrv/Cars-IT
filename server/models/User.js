f997574009e4c23c95b663b7e08d9d840ca83291
Erik Todorov <eriktdrv@gmail.com>
2022-11-26 09:50:47 +0200

    Updating User validation

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
     username: {
         required: true,
         type: String,
+        minlength: [4, 'Username should have at least 4 characters!'],
+        maxlength: [10, 'Username cannot have more than 10 characters!'],
     },
     email: {
         required: true,
@@ -12,7 +14,9 @@ const userSchema = new mongoose.Schema({
     password: {
         required: true,
         type: String,
-    }
+        minlength: [6, 'Password should have at least 6 characters!'],
+        maxlength: [12, 'Password cannot have more than 12 characters!'],
+    },
 })
 userSchema.pre('save', function (next) {
     bcrypt.hash(this.password, 10)
