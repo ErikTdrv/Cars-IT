537743ce09725371dca097e128650d51a627d3b0
Erik Todorov <eriktdrv@gmail.com>
2022-11-27 16:25:05 +0200

    Creating navigation bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
