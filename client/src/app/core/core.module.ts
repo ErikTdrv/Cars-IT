fdf05dd28155bc4fba3595b546544c89edc9ad33
Erik Todorov <eriktdrv@gmail.com>
2022-11-29 15:51:40 +0200

    home component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, HomeComponent]
 })
 export class CoreModule { }
