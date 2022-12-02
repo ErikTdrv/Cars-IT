a200453ee84c1b27caf39b3855f249e7343c047e
Erik Todorov <eriktdrv@gmail.com>
2022-12-02 20:23:35 +0200

    Adding all cars component

---
+++ client/src/app/car/all-cars/all-cars.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllCarsComponent } from './all-cars.component';
+
+describe('AllCarsComponent', () => {
+  let component: AllCarsComponent;
+  let fixture: ComponentFixture<AllCarsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllCarsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AllCarsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/car/all-cars/all-cars.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-cars',
+  templateUrl: './all-cars.component.html',
+  styleUrls: ['./all-cars.component.css']
+})
+export class AllCarsComponent {
+
+}
