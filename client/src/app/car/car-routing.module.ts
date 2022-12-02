d1c81fd4d9c5d8498c47eb521a486a7dc07d4482
Erik Todorov <eriktdrv@gmail.com>
2022-12-02 19:00:10 +0200

    Adding all cars component

--- client/src/app/car/car-routing.module.ts
+++ client/src/app/car/car-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AddCarComponent } from "./add-car/add-car.component";
+import { AllCarsComponent } from "./all-cars/all-cars.component";
 
 const routes: Routes = [
     {
         path: 'add',
         component: AddCarComponent,
+    },
+    {
+        path: 'all-cars',
+        component: AllCarsComponent,
     }
 ]
 
--- client/src/app/car/car.module.ts
+++ client/src/app/car/car.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AddCarComponent } from './add-car/add-car.component';
 import { CarRoutingModule } from './car-routing.module';
+import { AllCarsComponent } from './all-cars/all-cars.component';
 
 
 
 @NgModule({
   declarations: [
-    AddCarComponent
+    AddCarComponent,
+    AllCarsComponent
   ],
   imports: [
     CommonModule,
