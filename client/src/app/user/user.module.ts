524289147dea2b1cea0721bac7669938c67404a5
Erik Todorov <eriktdrv@gmail.com>
2022-12-03 19:27:02 +0200

    Update user.module.ts

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
