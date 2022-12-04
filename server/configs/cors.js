01a6fbdcf6df468c36afd478865b3177d4157d0c
Erik Todorov <eriktdrv@gmail.com>
2022-12-04 14:31:41 +0200

    Fixing cors bugs

--- server/configs/cors.js
+++ server/configs/cors.js
@@ -2,6 +2,5 @@ module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
-
     next();
 };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const cors = require('./configs/cors');
-
+const manualCors = require('./configs/cors');
+const cors = require('cors')
 const server = require('./environment');
 const initDatabase = require('./configs/database');
 const routes = require('./routes');
 const { authMiddleware } = require('./middlewares/auth');
 
-app.use(cors());
+// app.use(cors())
+app.use(manualCors());
 app.use(express.json())
 app.use(authMiddleware)
 app.use(routes)
