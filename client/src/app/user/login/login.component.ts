182915a39ced9b97b69c1c9e538cfa35866982e7
Erik Todorov <eriktdrv@gmail.com>
2022-12-04 10:55:20 +0200

    Added login functionality & validation

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(private userService: UserService, private router: Router){}
+  errors: string | undefined = undefined;
+  login(form: NgForm): void{
+    this.userService.login(form.value).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => {
+        this.errors = err.error.error
+      }
+    })
+  }
 }
