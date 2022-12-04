d629c1d5c60c9849e628c8f828081e13d9e4a0c1
Erik Todorov <eriktdrv@gmail.com>
2022-12-04 02:10:22 +0200

    Adding validation

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { emailValidator, passwordValidator } from 'src/app/shared/validator';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form!: FormGroup;
+  errors: string | undefined = undefined;
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, emailValidator]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      rePassword: ['', [Validators.required, passwordValidator]]
+    });
+  }
+
+  register(form: NgForm): void{
+    console.log(form)
+  }
 
 }
