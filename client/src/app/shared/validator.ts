782983dd9c04eda5ca833fd4ae0f6a447c758eaa
Erik Todorov <eriktdrv@gmail.com>
2022-12-07 00:20:15 +0200

    Updating email validation

--- client/src/app/shared/validator.ts
+++ client/src/app/shared/validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function emailValidator(control: AbstractControl): ValidationErrors | null {
   if (!control.value) { return null; }
-  return /^[a-zA-Z0-9\.-]{6,}@gmail\.(bg|com)$/.test(control.value) ? null : {
+  return /^[a-zA-Z0-9\.-]{4,}@[a-z]+\.[a-z]+$/.test(control.value) ? null : {
     invalidEmail: true
   };
 }
