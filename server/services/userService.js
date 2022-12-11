d2b85375d165bc0018b6d6661fbd412cca651f2f
Erik Todorov <eriktdrv@gmail.com>
2022-12-12 00:45:18 +0200

    update

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const server = require('../environment')
 const User = require('../models/User')
 
