1c6625661552bbe69bdfa91870c40037127938f7
Erik Todorov <eriktdrv@gmail.com>
2022-12-12 11:44:41 +0200

    Update special.js

--- server/services/special.js
+++ server/services/special.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// npm i node-fetch
 
 const getByVin = async (vin) => {
     try {
