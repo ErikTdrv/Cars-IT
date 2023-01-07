13ddaa1efa3cae138cc027708ca985af9d2995ec
Erik Todorov <eriktdrv@gmail.com>
2023-01-07 15:22:00 +0200

    Update register.component.ts

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -38,16 +38,16 @@ export class RegisterComponent {
   }
   async register(avatarImg: any){
     const file: File = avatarImg.files[0];
-    // if(file){
-    //   let base64 = await this.convertToBase64(file)
-    //   this.form.value.avatarImg = base64;
-    // }
-    // this.userService.register(this.form.value).subscribe({
-    //   next: () => this.router.navigate(['/']),
-    //   error: (err) => {
-    //     this.errors = handleError(err.error?.error)
-    //   }
-    // })
+    if(file){
+      let base64 = await this.convertToBase64(file)
+      this.form.value.avatarImg = base64;
+    }
+    this.userService.register(this.form.value).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => {
+        this.errors = handleError(err.error?.error)
+      }
+    })
   }
   async onChange(avatar: any){
     document.getElementById('avatar')?.classList.add('avatarImg')
