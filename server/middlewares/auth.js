b7106b7bbbbdfd3aff8c6d38d332d85ee0b1e7c5
Erik Todorov <eriktdrv@gmail.com>
2023-02-04 18:33:56 +0200

    Making cookie middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,20 +1,20 @@
 const { validateToken } = require("../services/user");
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['x-authorization'];
-    if(token){
+    // const token = req.headers['x-authorization']; -- Storing in local storage
+    const cookie = req.cookies?.auth;
+    if(cookie){
         try {
-            const user = validateToken(token);
+            const user = validateToken(cookie);
             req.user = {
                 'email': user.email,
                 'username': user.username,
                 '_id': user._id,
-                token
+                cookie
             }
         } catch (error) {
             res.json(error)
             console.log(error)
-            
         }
     }
     next();
