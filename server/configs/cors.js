980b506e5f2e311091d67a94b4052de5c858d5ce
Erik Todorov <eriktdrv@gmail.com>
2023-02-05 19:02:19 +0200

    Updating cors

--- server/configs/cors.js
+++ server/configs/cors.js
@@ -1,7 +1,8 @@
 module.exports = () => (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', 'https://carsit-fe.ew.r.appspot.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Content-Type, X-Authorization, X-RapidAPI-Key, X-RapidAPI-Host');
+    res.header("Access-Control-Allow-Headers", 'Content-Type, X-Authorization, X-RapidAPI-Key, X-RapidAPI-Host, Origin, X-Requested-With, Accept, Cookie"');
     next();
 };
