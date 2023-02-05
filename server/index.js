109facfd0ddc1457c5803ee37f4174d6cca635ea
Erik Todorov <eriktdrv@gmail.com>
2023-02-05 19:02:01 +0200

    Updating cors

--- server/index.js
+++ server/index.js
@@ -11,15 +11,21 @@ const manualCors = require('./configs/cors')
 startServer()
 async function startServer() {
     try {
+        // app.use(manualCors())
+        app.use(cors({
+            origin: 'https://carsit-fe.ew.r.appspot.com',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'Origin', 'X-Requested-With', 'Accept', 'Cookie'],
+            credentials: true,
+            allowedHeaders: ['Content-Type, X-Authorization, X-RapidAPI-Key, X-RapidAPI-Host'],
+        }))
+
         app.use(cookieParser())
         app.use(bodyParser.json({ limit: '50mb' }));
         app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-        app.use(cors({ credentials: true, origin: ['http://localhost:4200', 'https://carsit-fe.ew.r.appspot.com'], allowedHeaders: ['Content-Type, X-Authorization, X-RapidAPI-Key, X-RapidAPI-Host'] }))
-        // app.use(manualCors())
         app.use(express.json())
         app.use(authMiddleware)
         app.use(routes)
-        
+
         //Initializing database
         let port = process.env.PORT || 3030
         app.listen(port, () => console.log(`Server listening on http://localhost:${port}`))
